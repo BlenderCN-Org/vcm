8c602ded3030d00f9902c6546d608334dcd1d331
Vladimir Zarypov <krre31@gmail.com>
2016-03-14 19:30:59 +0200

    added __init__.py

---
+++ __init__.py
@@ -0,0 +1,26 @@
+bl_info = {
+    "name": "Visual Code Machine",
+    "description": "IDE for visual programming by Blender",
+    "author": "Vladimir Zarypov (krre)",
+    "version": (0, 1),
+    "blender": (2, 65, 0),
+    "location": "Search > Visual Code Machine",
+    "warning": "",
+    "wiki_url": "http://github.com/krre/vcm",
+    "tracker_url": "",
+    "support": "COMMUNITY",
+    "category": "Development"
+}
+
+import bpy
+
+def register():
+    bpy.utils.register_module(__name__)
+
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+
+
+if __name__ == "__main__":
+    register()
