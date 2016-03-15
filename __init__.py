3f8500ecc1e3a9129bfd51de5f5103941db0bb4a
Vladimir Zarypov <krre31@gmail.com>
2016-03-15 08:01:09 +0200

    fixed version

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Visual Code Machine",
     "description": "IDE for visual programming by Blender",
     "author": "Vladimir Zarypov (krre)",
-    "version": (0, 1),
+    "version": (0, 1, 0),
     "blender": (2, 65, 0),
     "location": "Search > Visual Code Machine",
     "warning": "",
