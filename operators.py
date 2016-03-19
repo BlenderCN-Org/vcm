5352d5ffdc8f625a573894e715b1f223c075bd84
Vladimir Zarypov <krre31@gmail.com>
2016-03-19 12:12:42 +0200

    reset viewport

--- operators.py
+++ operators.py
@@ -13,4 +13,22 @@ class VCMOperator(Operator):
             obj.select = True
         bpy.ops.object.delete()
 
+        # Reset viewport
+        for area in bpy.context.screen.areas:
+            if area.type == 'VIEW_3D':
+                for space in area.spaces:
+                    if space.type == 'VIEW_3D':
+                        region = space.region_3d
+
+                        region.view_location.x = 0
+                        region.view_location.y = 0
+                        region.view_location.z = 0
+
+                        region.view_rotation.w = 1
+                        region.view_rotation.x = 0
+                        region.view_rotation.y = 0
+                        region.view_rotation.z = 0
+
+                        region.view_distance = 10
+
         return {'FINISHED'}
